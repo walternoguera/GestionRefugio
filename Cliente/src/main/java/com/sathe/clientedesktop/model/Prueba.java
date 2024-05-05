31514d796721900a967687466723f4b7e5eebe46
walternoguera <walter.morel.noguera@gmail.com>
2024-05-05 23:27:43 +0200

    Estructura

---
+++ Cliente/src/main/java/com/sathe/clientedesktop/model/Prueba.java
@@ -0,0 +1,13 @@
+package com.sathe.clientedesktop.model;
+
+public class Prueba {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String nombre = "Walter";
+		
+		System.out.println(nombre);
+
+	}
+
+}
